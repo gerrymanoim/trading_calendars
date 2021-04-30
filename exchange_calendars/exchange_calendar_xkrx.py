439e7bfb100fe0a81172a6311ec3ebb2e6da1ddb
Yunseong Hwang <kika1492@gmail.com>
2021-04-30 09:16:29 +0900

    Remove unused import

--- exchange_calendars/exchange_calendar_xkrx.py
+++ exchange_calendars/exchange_calendar_xkrx.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from datetime import datetime, time
+from datetime import time
 
 import pandas as pd
 
